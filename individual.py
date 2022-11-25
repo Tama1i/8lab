0df5595f3e6631f1a11cba85a31661f17e89020b
Tama1i <ryazantseff.matvei@yandex.ru>
2022-11-25 19:03:10 +0300

    fixx1

--- individual.py
+++ individual.py
@@ -3,19 +3,15 @@
 import sys
 
 if __name__ == '__main__':
-    a1 = (int(input("team1 ")),"team1")
-    a2 = (int(input("team2 ")),"team2")
-    a3 = (int(input("team3 ")),"team3")
+    a = ((int(input("team1 ")),"team1"),(int(input("team2 ")),"team2"),(int(input("team3 ")),"team3"))
     m = [input(" 1 "),input(" 2 "),input(" 3 ")]
     p = True
-
+    
     for i,val in enumerate(m):
-        if m[i] == a1[1]:
-            m[i] = int(a1[0])
-        if m[i] == a2[1]:
-            m[i] = int(a2[0])
-        if m[i] == a3[1]:
-            m[i] = int(a3[0])
+        for j in a:
+            if m[i] == j[1]:
+                m[i] = int(j[0])
+            
     for i in range(0,1):
         if int(m[i]) < int(m[i+1]):
             p = False
