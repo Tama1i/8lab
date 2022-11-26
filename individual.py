48e040075e50fd8d921e76a79717f57e6a045949
Tama1i <ryazantseff.matvei@yandex.ru>
2022-11-26 09:51:37 +0300

    fiX

--- individual.py
+++ individual.py
@@ -6,15 +6,17 @@ if __name__ == '__main__':
     a = ((int(input("team1 ")),"team1"),(int(input("team2 ")),"team2"),(int(input("team3 ")),"team3"))
     m = [input(" 1 "),input(" 2 "),input(" 3 ")]
     p = True
+    k = 0
     
     for i,val in enumerate(m):
         for j in a:
             if m[i] == j[1]:
                 m[i] = int(j[0])
             
-    for i in range(0,1):
-        if int(m[i]) < int(m[i+1]):
+    for i,val in enumerate(m):
+        if int(k < int(val)):
             p = False
+        k = m[i]
         
     if  p == False:
         print("ne pravilno")
